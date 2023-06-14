f65ba9ef1e5ffebe9a68d3d592880367c12520be
Cohen Ariel Eitan <a.cohen@etec.local>
2023-06-14 12:42:20 -0300

    daisy

---
+++ hockey_django/hockeyweb/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "../src/component/Navbar";
+import Footer from "../src/component/Footer";
+import Hero from "../src/component/Hero";
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Hero/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
