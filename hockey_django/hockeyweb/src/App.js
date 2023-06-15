58c836ef55703bfeaf90d19e7df0c6f452b134c5
Cohen Ariel Eitan <a.cohen@etec.local>
2023-06-15 12:30:07 -0300

    login

--- hockey_django/hockeyweb/src/App.js
+++ hockey_django/hockeyweb/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from "../src/component/Navbar";
-import Footer from "../src/component/Footer";
-import Hero from "../src/component/Hero";
+import NavBar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Home from "./component/home";
+import Login from "./component/Login"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './component/Navbar';
+
+
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <Hero/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
   );
 }
--- hockey_django/hockeyweb/src/component/Navbar.js
+++ hockey_django/hockeyweb/src/component/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
     return (
@@ -20,7 +21,9 @@ const Navbar = () => {
                                 Profile
                             </a>
                             </li>
-                            <li><a>Settings</a></li>
+                            <Link to="Login">
+                                Loginn
+                            </Link>
                             <li><a>Logout</a></li>
                         </ul>
                     </div>
